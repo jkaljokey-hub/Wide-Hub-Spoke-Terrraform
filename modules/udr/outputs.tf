2185556f8c79053c8e589743452ae363a54a3133
abubakar <jkaljokey@gmail.com>
2026-03-19 21:00:36 +0300

    UDR

--- modules/udr/outputs.tf
+++ modules/udr/outputs.tf
@@ -1,8 +1,3 @@
-
-output "firewall_public_ip" {
-  value = azurerm_public_ip.firewall_pip.ip_address
-}
-
-output "firewall_id" {
-  value = azurerm_firewall.firewall.id
+output "route_table_id" {
+  value = azurerm_route_table.rt.id
 }
--- modules/udr/variables.tf
+++ modules/udr/variables.tf
@@ -1,19 +1,5 @@
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "route_table_name" {
-  type = string
-}
-
-variable "subnet_id" {
-  type = string
-}
-
-variable "firewall_private_ip" {
-  type = string
-}
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "route_table_name" { type = string }
+variable "subnet_id" { type = string }
+variable "firewall_private_ip" { type = string }
